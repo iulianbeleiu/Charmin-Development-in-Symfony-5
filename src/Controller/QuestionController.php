5246a33bb32d0655612a11d9f196bd77ea85f247
Iulian Beleiu <i.beleiu@plan-net.com>
2020-08-31 10:20:15 +0300

    create the first twig template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @return Response
@@ -25,9 +26,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Show Question: "%s"',
-            ucfirst(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucfirst(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
     }
 }
